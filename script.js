b31acd8764cec9d1278003420bd5240e158a7201
MirzaAyon <ayonmirza98@gmail.com>
2022-07-27 21:16:15 +0600

    readme added

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("Ayon"); //js thik thak moto connected 
+
+const handleSearch = () => {
+    // console.log('Ayon');
+    const keyword = document.getElementById('keyword');
+    // console.log(keyword); html ashbe
+    console.log(keyword.value); //ekhn input nile value ashbe
+};
