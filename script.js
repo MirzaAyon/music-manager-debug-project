dc6f933fd8382c0a60cd6f51916ab81f184a9bcc
MirzaAyon <ayonmirza98@gmail.com>
2022-07-27 21:54:03 +0600

    script added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // console.log("Ayon"); //js thik thak moto connected 
 
 const handleSearch = () => {
-    // console.log('Ayon');
+    // console.log('Ayon'); //click korle ayon ashbe
     const keyword = document.getElementById('keyword');
-    // console.log(keyword); html ashbe
+    // console.log(keyword); html ashbe/pura html ta pai
     console.log(keyword.value); //ekhn input nile value ashbe
 };
