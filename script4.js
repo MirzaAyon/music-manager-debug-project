c726b6156a38e523cd7139f9ec3c92898c9673f3
MirzaAyon <ayonmirza98@gmail.com>
2022-07-27 22:33:25 +0600

    script4 with api details added and readme updated

---
+++ script4.js
@@ -0,0 +1,27 @@
+const elementById = (id) => {
+
+    return document.getElementById('keyword');
+}
+
+const handleSearch = () => {
+
+    const keyword = elementById('keyword');
+
+
+    const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`; //value na dile error dibe
+    //https na dile error dibe
+    console.log(url);
+    // fetch(url).then((res) => res.json()).then((data) => console.log(data));
+    // etake chaile evabeo likha jae eke chaining bole
+    // line gular kothao ; dewa hoyni tar mane line break hyni
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+};
+//ekhn data ta inspact e showing
+//coldplay diye search dile ashbe
+//onno kichu dile null dibe
+//black dile kichu dibe na just api dibe
+//json data json theke niye postman e try kore dekhbo
+//shob thik thak dekhabe
+//coldplay and aurora diye search krbo shob info dibo
